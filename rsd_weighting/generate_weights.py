6ea54d255e8a6de3856f42b828e76a8223814541
Claire Lamman <cmlamman@gmail.com>
2024-02-13 14:09:15 -0800

    minor module fixes

--- rsd_weighting/generate_weights.py
+++ rsd_weighting/generate_weights.py
@@ -3,6 +3,8 @@ from pycorr import TwoPointCorrelationFunction
 import random
 import numpy as np
 from scipy.interpolate import interp2d
+from scipy import stats
+import astropy.units as u
 
 
 from astropy.cosmology import LambdaCDM, z_at_value
@@ -31,7 +33,7 @@ def get_mu(r_p, r_par):
 def bin_pair_results(pair_results, nrp_bins=100, npar_bins=150, rp_max=20, par_max=30):
     '''
     Input: pair_results, a table of computed galaxy pairs and their relative alignment. Containing columns r_p, r_par, s_par, and e1_rel
-    For each of the 2D bins in r_p and r_par, this function calculates the IA signal and correlation function.
+    For each of the 2D bins in r_p and r_par, this function calculates the IA signal.
     Returns the IA signal and correlation function used. Both are 2D arrays of size (nrp_bins, npar_bins)
     '''
     rp_bins = np.linspace(0, rp_max, nrp_bins+1)
@@ -72,10 +74,19 @@ def get_rsd_ia_weights(cf_binned, ia_binned):
     # find the total signal in each bin of r_p (i.e. "rp slice") and the the weight in each r_par bin for each slice
     weight_array = np.zeros_like(cf_binned)
     
-    slice_sum = np.sum(ia_binned**2 / (1 + cf_binned), axis=1)
-    slice_weights = (ia_binned / (1 + cf_binned)) / slice_sum[:, np.newaxis]
+    slice_sum = np.sum(ia_binned**2 / (1 + cf_binned), axis=1)  # for each bin in r_p
+    slice_weights = (ia_binned / (1 + cf_binned)) / slice_sum[:, np.newaxis]  # for each r_par bin in each r_p slice
     
     # add the weights to the weight array
     weight_array = slice_weights
     
     return weight_array
+
+def get_projected_weighted_signal(signal_2d, weights_2d):
+    '''
+    Input: signal_2d, a 2D array of the signal binned in r_p and r_par. size (nrp_bins, npar_bins)
+    weights_2d, a 2D array of the weights binned in r_p and r_par. size (nrp_bins, npar_bins)
+    Returns: a 1D array of the weighted signal, binned in r_p
+    '''
+    weighted_average = np.nansum(signal_2d * weights_2d, axis=1) / np.nansum(weights_2d, axis=1)
+    return weighted_average
