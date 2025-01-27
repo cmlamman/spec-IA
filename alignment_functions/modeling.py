04f77da5628b90f7caa7aceb0667ded84b6588b9
Claire Lamman <cmlamman@gmail.com>
2025-01-26 18:59:20 -0800

    improving modeling function and adding kz spline

--- alignment_functions/modeling.py
+++ alignment_functions/modeling.py
@@ -138,7 +138,7 @@ def compute_rel_e_model(rel_e_measurement, wp_measurement, pimax_values, b_gal, 
     INPUT - REQUIRED:
     ------------------
     rel_e_measurement: dictonary or DataFrame of projected relative ellipticity measurement between a shape catalog and a tracer catalog.
-        must contain columns 'R_bin_min', 'R_bin_max', 'relAng_plot'.
+        must contain columns 'R_bin_min', 'R_bin_max', ''.
     wp: dictonary or DataFrame of projected cross-correlation function between a shape and a tracer catalog.
         must contain columns 'R_bin_min', 'R_bin_max', 'wp'.
     Rbins: array of shape (n+1,). Bin edges that rel_e_measurment and wp were made in [Mpc/h]
@@ -201,7 +201,7 @@ def compute_rel_e_model(rel_e_measurement, wp_measurement, pimax_values, b_gal, 
         
     R_bin_centers = (rel_e_measurement['R_bin_min']+rel_e_measurement['R_bin_max'])/2
     R_bin_edges = np.append(np.asarray(rel_e_measurement['R_bin_min']), rel_e_measurement['R_bin_max'][-1])
-    ia_measurement = rel_e_measurement['relAng_plot'] - randoms
+    #ia_measurement = rel_e_measurement['relAng_plot'] - randoms
     wp_values = wp_measurement['wp']
 
     def get_wp(R):
@@ -241,3 +241,93 @@ def compute_rel_e_model(rel_e_measurement, wp_measurement, pimax_values, b_gal, 
     r_bin_centers = np.asarray(r_bin_centers)
     
     return r_bin_centers, model_estimates*D_norm
+
+
+def get_rel_e_model(R_bin_min, R_bin_max, pimax_values, wp_values, b_gal, z = 0.8, 
+                 PS_min = 1e-4, PS_max = 2e2, PS_data = abacus_ps_nl, PS_z = 0.8, precomputed_kz_integral_paths = [str(kzs) for kzs in kz_spline_paths], warning_handling='once'):
+    '''
+    UPDATED VERSION OF ABOVE
+    Compute a model prediction at each bin of projected separation. This code assumes a tau value of 1.
+    
+    INPUT - REQUIRED:
+    ------------------
+    'R_bin_min', 'R_bin_max': floats. Bin edges of the projected separation
+    pimax_values: float or array of shape (n,). Line-of-sight distance that rel_e_measurement and wp were made [Mpc/h]
+    wp: projected cross-correlation function between a shape and a tracer catalog. Must be made in the same bins as the rel_e_measurement
+    b_gal: float. bias of the galaxy catalog used for tracers
+    z: float. redshift of galaxies used in measurement
+    
+    INPUT - OPTIONAL:
+    ------------------
+    PS_min, PS_max: floats. Range of k to use for the power spectrum [h/Mpc]
+    PS_data: dictonary or DataFrame of matter power spectrum values. Must contain columns 'k' and 'P'. Default is a non-linear matter power spectrum from AbacusSummit.
+    PS_z = float. redshift of the power spectrum. Default is 0.8.
+    precomputed_kz_integral_paths: list of paths to precomputed values of the kz integral afor given pi values, made with the above power spectum. 
+        Must be formatted as the output of precompute_kz_integrand.
+    '''
+    warnings.filterwarnings(warning_handling)
+        
+    # if the redshift of the measurement and power spectrum are different, adjust using growth factor
+    D_norm = 1
+    if z != PS_z:
+        D_norm = (D(z) / D(PS_z))**2
+
+        
+    # read in the pre-computed value of the kz integral (made with precompute_kz_integral)
+    splines = [np.load(path, allow_pickle=True) for path in precomputed_kz_integral_paths]
+    pimax_kz_values = [round(float(path.split('_')[-1].split('.npy')[0]), 2) for path in precomputed_kz_integral_paths]
+
+    def get_kz_integral_spl(K, pimax, b_gal):
+        try:
+            i_to_use = list(pimax_kz_values).index(round(pimax, 2))
+        except ValueError:
+            print('Pimax value of ', pimax, 'not found in pre-computed values. Use precompute_kz_integral() to generate first. Continuing with a pimax value of 30.0 Mpc/h')
+            pimax = 30
+            i_to_use = list(pimax_kz_values).index(round(pimax, 2))
+        front_constant = b_gal * pimax / np.pi
+        return front_constant * (interpolate.splev(np.log10(K), splines[i_to_use]))
+    
+    # for last ingetral
+    def K_integrand(K, Rmin, Rmax, pimax, b_gal, PS_min = 10**-4, PS_max = 100):
+        kz_integral = get_kz_integral_spl(K, pimax, b_gal)
+        return K * get_fancyJ(Rmin, Rmax, K) * kz_integral
+
+    def get_model_est(Rmin, Rmax, pimax, b_gal, tau=1, PS_min = 10**-4, PS_max = 100):
+        
+        bar_wp = get_bar_wp(Rmin, Rmax)
+        K_integral = scipy.integrate.romberg(K_integrand, a=PS_min, b=PS_max, args=[Rmin, Rmax, pimax, b_gal], rtol=1.48e-8, divmax=15) # integrate over K
+        
+        return tau * K_integral / (2*pimax + bar_wp)
+        
+    R_bin_centers = (R_bin_min+R_bin_max)/2
+    R_bin_edges = np.append(np.asarray(R_bin_min), R_bin_max[-1])
+
+    def get_wp(R):
+        return np.interp(R, R_bin_centers, wp_values)
+
+    # getting \bar{w_p}, per-bin
+    def wp_integrand(R):
+        return R * get_wp(R)
+
+    def get_bar_wp(Rmin, Rmax):
+        wp_integral = scipy.integrate.romberg(wp_integrand, a=Rmin, b=Rmax, rtol=1.48e-8, divmax=15) # integrate over R
+        return (2 / (Rmax**2 - Rmin**2)) * wp_integral
+    
+    # for last ingetral
+    def K_integrand(K, Rmin, Rmax, pimax, b_gal, PS_min = 10**-4, PS_max = 100):
+        kz_integral = get_kz_integral_spl(K, pimax, b_gal)
+        return K * get_fancyJ(Rmin, Rmax, K) * kz_integral
+    
+    def get_model_est(Rmin, Rmax, pimax, b_gal, tau=1, PS_min = 10**-4, PS_max = 100):
+        bar_wp = get_bar_wp(Rmin, Rmax)
+        K_integral = scipy.integrate.romberg(K_integrand, a=PS_min, b=PS_max, args=[Rmin, Rmax, pimax, b_gal], rtol=1.48e-8, divmax=15) # integrate over K
+        return -tau * K_integral / (2*pimax + bar_wp)
+    
+    # computing the model prediction in each bin of projected separation
+    model_estimates = []
+    for i in range(len(R_bin_min)):
+        model_est = get_model_est(R_bin_min[i], R_bin_max[i], 100, b_gal=b_gal, tau=1)
+        model_estimates.append(model_est)
+    model_estimates = np.asarray(model_estimates)
+    
+    return model_estimates*D_norm
