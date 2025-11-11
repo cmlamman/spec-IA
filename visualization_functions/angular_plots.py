d9f92d15cb8306c5b1c3be05f0b8e6ca1afb93f6
Claire Lamman <cmlamman@gmail.com>
2025-11-11 12:16:22 -0800

    added option to average over mu in plotting visualization_functions/angular_plots.py

--- visualization_functions/angular_plots.py
+++ visualization_functions/angular_plots.py
@@ -4,7 +4,7 @@ from astropy.io import fits
 # make all fonts larger
 plt.rcParams.update({'font.size': 14})
 
-def plot_MIA_mu_bins(result_paths, estimator, title=None, save_path=None, cmap='seismic', ylim=None, vlim=None, mu_sym=False):
+def plot_MIA_mu_bins(result_paths, estimator, title=None, save_path=None, cmap='seismic', ylim=None, vlim=None, mu_sym=False, plot_mu_averaged=False):
     """
     Plots the MIA results measured in bins of separation s and angle mu wrt the line of sight.
     
@@ -49,13 +49,22 @@ def plot_MIA_mu_bins(result_paths, estimator, title=None, save_path=None, cmap='
     stderr1d_s = np.nanmean(stderr2d, axis=1)
     s_bin_middles = (s_bins[1:]+s_bins[:-1])/2
     
-    
-    fig = plt.figure(figsize=(15,6))
+    if plot_mu_averaged:
+        fig = plt.figure(figsize=(17,4))
+        # reduce horizontal spacing when the mu-averaged (middle) plot is present
+        plt.subplots_adjust(wspace=0.4)
+    else:
+        fig = plt.figure(figsize=(15,6))
+        
+        
     # make title for entire figure
     fig.suptitle(title, fontsize=16)
-    
+
     # averaged over angular bins
-    plt.subplot(1,2,1)
+    if plot_mu_averaged:
+        plt.subplot(1,3,1)
+    else:
+        plt.subplot(1,2,1)
     plt.errorbar(s_bin_middles, mean1d_s*s_bin_middles, yerr=stderr1d_s*s_bin_middles, fmt='o', capsize=4)
     plt.xscale('log')
     plt.xlabel(r'3D separation $s$ [$h^{-1}$ Mpc]')
@@ -64,6 +73,40 @@ def plot_MIA_mu_bins(result_paths, estimator, title=None, save_path=None, cmap='
     plt.axhline(0, color='grey', linestyle='--');
     if ylim is not None:
         plt.ylim(ylim)
+        
+    # optional mu plot
+    if plot_mu_averaged:
+        plt.subplot(1,3,2)
+        # averaging in s to get mu dependence
+        mean1d_mu = np.nanmean(mean2d, axis=0)
+        stderr1d_mu = np.nanmean(stderr2d, axis=0)
+        mu_bin_middles = (mu_bins[1:]+mu_bins[:-1])/2
+        if mu_sym:
+            # average over mu and -mu
+            mean1d_mu_flipped = mean1d_mu[::-1]
+            mean1d_mu = 0.5 * (mean1d_mu + mean1d_mu_flipped)
+            stderr1d_mu_flipped = stderr1d_mu[::-1]
+            stderr1d_mu = 0.5 * (stderr1d_mu + stderr1d_mu_flipped)
+            # only take positive mu values
+            mean1d_mu = mean1d_mu[mu_bin_middles>=0]
+            stderr1d_mu = stderr1d_mu[mu_bin_middles>=0]
+            mu_bin_middles = mu_bin_middles[mu_bin_middles>=0]
+        plt.errorbar(mu_bin_middles, mean1d_mu, yerr=stderr1d_mu, fmt='o', capsize=4)
+        plt.xlabel(r'$\mu$')
+        plt.ylabel(r'MIA$_{'+estimator+r'}(\mu)$');
+        plt.title(None);
+        # draw thin grey line at y=0
+        plt.axhline(0, color='grey', linestyle='--');
+        # find the average and error of the mean over all mu bins
+        overall_mean = np.nanmean(mean1d_mu)
+        overall_stderr = np.nanmean(stderr1d_mu)
+        # print out
+        print(f'Overall mean MIA_{estimator}: {overall_mean:.2e} ± {overall_stderr:.0e}')
+        # mean within abs(mu)<.9
+        abs_mu_mask = np.abs(mu_bin_middles) < 0.9
+        mean_absmu = np.nanmean(mean1d_mu[abs_mu_mask])
+        stderr_absmu = np.nanmean(stderr1d_mu[abs_mu_mask])
+        print(f'Mean MIA_{estimator} for |mu|<0.9: {mean_absmu:.2e} ± {stderr_absmu:.0e}')
     
     
     # RADIAL PLOT
@@ -71,11 +114,18 @@ def plot_MIA_mu_bins(result_paths, estimator, title=None, save_path=None, cmap='
     # monkeypatch plt.subplot so the right (1,2,2) subplot is created slightly larger
     _orig_subplot = plt.subplot
     def _subplot_wrapper(*args, **kwargs):
-        # detect call to plt.subplot(1,2,2, ...)
-        if len(args) >= 3 and args[0] == 1 and args[1] == 2 and args[2] == 2:
-            # rect = [left, bottom, width, height] (tweak these to adjust size)
-            rect = [0.52, 0.064, 0.40, 0.80]
-            return plt.axes(rect, **kwargs)
+        # support calls like plt.subplot(nrows, ncols, index, ...)
+        if len(args) >= 3:
+            nrows, ncols, idx = args[0], args[1], args[2]
+            # tweak for 1x2 layout: make right subplot larger
+            if nrows == 1 and ncols == 2 and idx == 2:
+                rect = [0.52, 0.064, 0.40, 0.80]
+                return plt.axes(rect, **kwargs)
+            # tweak for 1x3 layout: reduce space between 2nd and 3rd by shifting 3rd left
+            if nrows == 1 and ncols == 3 and idx == 3:
+                # rect = [left, bottom, width, height]
+                rect = [0.54, 0.064, 0.40, 0.80]
+                return plt.axes(rect, **kwargs)
         return _orig_subplot(*args, **kwargs)
     plt.subplot = _subplot_wrapper
     data = mean2d.T
@@ -89,7 +139,10 @@ def plot_MIA_mu_bins(result_paths, estimator, title=None, save_path=None, cmap='
     # mask NaNs
     data_ma = np.ma.masked_invalid(data)
     
-    ax = plt.subplot(1,2,2, projection='polar')
+    if plot_mu_averaged:
+        ax = plt.subplot(1,3,3, projection='polar')
+    else:
+        ax = plt.subplot(1,2,2, projection='polar')
     ax.set_theta_zero_location('N')
     ax.set_theta_direction(-1)
 
@@ -105,8 +158,6 @@ def plot_MIA_mu_bins(result_paths, estimator, title=None, save_path=None, cmap='
     # create mirrored angular coordinates and mirrored data (reflection about vertical axis)
     Theta_mirror = np.pi - np.mod(-Theta, 2*np.pi)         # theta -> -theta (wrapped to [0,2pi))
     data_mirror = data_ma[::-1, :]                 # flip angular rows to match reflection
-
-    # plot mirrored overlay with some transparency
     pcm2 = ax.pcolormesh(Theta_mirror, R, data_mirror, cmap=cmap, vmin=-vmax, vmax=vmax,
                         shading='auto')
 
@@ -150,6 +201,7 @@ def plot_MIA_mu_bins(result_paths, estimator, title=None, save_path=None, cmap='
     ax.set_xticklabels(mu_tick_labels)
     # remove radial labels
     ax.set_yticks([])
+    
     
     if save_path is not None:
         plt.savefig(save_path, bbox_inches='tight', dpi=300)
